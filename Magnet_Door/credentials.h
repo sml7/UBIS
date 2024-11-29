d6ca2a9989115ea7e8f952ac6d41ec882b762f63
Leon Schmid <leon.schmid@student.uni-siegen.de>
2024-11-29 17:13:28 +0100

    Moved files

---
+++ Magnet_Door/credentials.h
@@ -0,0 +1,9 @@
+// Blynk credentials
+#define BLYNK_TEMPLATE_ID "TMPL4AnEFpL5p"
+#define BLYNK_TEMPLATE_NAME "Quickstart Device"
+#define BLYNK_AUTH_TOKEN "qEjFtvtSFtEUyFgUdcyLCil66z_tXCc_"
+
+// WiFi credentials
+
+#define WIFI_SSID "Mostafa's Galaxy A52";       // Your WiFi SSID
+#define WIFI_PASS "knfm5166";               // Your WiFi password
